02de05d53c886f3d96c0f2523a02d8fb21d99b08
Gyan-coder103 <simabhuyan177@gmail.com>
2026-05-05 22:34:26 +0530

    Final

---
+++ Madlibs-Game/Madlibs.py
@@ -0,0 +1,23 @@
+# Madlibs game
+# word game where you create a story
+# by filling in blanks with random words
+
+name = input("Enter a person's name: ")
+place = input("Enter a name of a place: ")
+animal = input("Enter a name of an animal: ")
+adjective = input("Enter an adjective: ")
+verb = input("Enter a past tense verb: ")
+random_obj = input("Enter a name of an random object: ")
+food = input("Enter a name of a food item: ")
+emotion = input("Enter an emotion: ")
+verb2 = input("Enter another verb: ")
+body_part = input("Enter a name of a body part: ")
+print()
+print("Here is your funny story 😂")
+print()
+print(f"One day, {name} went to {place} to complete a secret mission.")
+print(f"Suddenly, a {adjective} {animal} appeared and {verb} right in front of them!")
+print(f"Without thinking, {name} grabbed a {random_obj} and threw it like a hero.")
+print(f"The {animal} got distracted by a {food} lying nearby and became very {emotion}.")
+print(f"“This is my chance!” shouted {name} and started to {verb2} as fast as possible.")
+print(f"But suddenly, they tripped over their own {body_part} and fell dramatically.")
